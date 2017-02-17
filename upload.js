790b3d8b6e35314f61399f531c7947dfcd6bc83b
Stephan Bönnemann <stephan@boennemann.me>
2017-02-17 18:58:00 +0100

    fix(upload): correctly **add** git origin

--- upload.js
+++ upload.js
@@ -9,7 +9,7 @@ module.exports = function upload () {
 
   if (!env.GH_TOKEN) throw new Error('Please provide a GitHub token as "GH_TOKEN" environment variable')
 
-  exec(`git remote set gk-origin https://${env.GH_TOKEN}@github.com/${env.TRAVIS_REPO_SLUG}`)
+  exec(`git remote add gk-origin https://${env.GH_TOKEN}@github.com/${env.TRAVIS_REPO_SLUG}`)
   exec('git config user.email "support@greenkeeper.io"')
   exec('git config user.name "greenkeeperio[bot]"')
   exec(`git push gk-origin HEAD:${env.TRAVIS_BRANCH} --force`)
