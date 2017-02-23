22cc10e6b3706d8cb824b8f5ba9143147864e711
Stephan Bönnemann <stephan@boennemann.me>
2017-02-23 21:53:19 +0100

    style(upload): consistent styling with update

--- upload.js
+++ upload.js
@@ -14,13 +14,21 @@ module.exports = function upload () {
   const config = pkg.greenkeeper || {}
   const branchPrefix = config.branchPrefix || 'greenkeeper/'
 
-  if (!env.TRAVIS_BRANCH.startsWith(branchPrefix)) return console.error('Not a Greenkeeper pull request.')
+  if (!env.TRAVIS_BRANCH.startsWith(branchPrefix)) {
+    return console.error('Not a Greenkeeper pull request.')
+  }
 
-  if (env.TRAVIS_BRANCH === (branchPrefix + 'initial')) return console.error('Not a Greenkeeper update pull request.')
+  if (env.TRAVIS_BRANCH === (branchPrefix + 'initial')) {
+    return console.error('Not a Greenkeeper update pull request.')
+  }
 
-  if (env.TRAVIS_COMMIT_RANGE) return console.error('Only running on first push of a new branch')
+  if (env.TRAVIS_COMMIT_RANGE) {
+    return console.error('Only running on first push of a new branch')
+  }
 
-  if (!env.TRAVIS_JOB_NUMBER.endsWith('.1')) return console.error('Only running on first build job')
+  if (!env.TRAVIS_JOB_NUMBER.endsWith('.1')) {
+    return console.error('Only running on first build job')
+  }
 
   exec(`git remote add gk-origin https://${env.GH_TOKEN}@github.com/${env.TRAVIS_REPO_SLUG}`)
   exec(`git push gk-origin HEAD:${env.TRAVIS_BRANCH}`)
