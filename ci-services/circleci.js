028b09f419b85c096bc13e7f199c9dd810fe0c3a
ethanrubio <ethanrubio@gmail.com>
2017-05-24 17:20:04 -0700

    Fix firstPush negation

--- ci-services/circleci.js
+++ ci-services/circleci.js
@@ -7,7 +7,7 @@ const env = process.env
 module.exports = {
   repoSlug: `${env.CIRCLE_PROJECT_USERNAME}/${env.CIRCLE_PROJECT_REPONAME}`,
   branchName: env.CIRCLE_BRANCH,
-  firstPush: gitHelpers.getNumberOfCommitsOnBranch(env.CIRCLE_BRANCH) === 1,
+  firstPush: gitHelpers.getNumberOfCommitsOnBranch(env.CIRCLE_BRANCH) !== 1,
   correctBuild: _.isEmpty(env.CI_PULL_REQUEST),
   uploadBuild: env.CIRCLE_NODE_INDEX === '0'
 }
