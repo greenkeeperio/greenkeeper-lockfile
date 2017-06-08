07913bfd04ff7ff4fe9c2041e3a4e0027a52f927
finnp <derfinn@gmail.com>
2017-06-08 16:28:49 +0200

    fix(circleci): use CIRCLE_PREVIOUS_BUILD_NUM to detect the first push

--- ci-services/circleci.js
+++ ci-services/circleci.js
@@ -1,13 +1,11 @@
 const _ = require('lodash')
 
-const gitHelpers = require('../lib/git-helpers')
-
 const env = process.env
 
 module.exports = {
   repoSlug: `${env.CIRCLE_PROJECT_USERNAME}/${env.CIRCLE_PROJECT_REPONAME}`,
   branchName: env.CIRCLE_BRANCH,
-  firstPush: gitHelpers.getNumberOfCommitsOnBranch(env.CIRCLE_BRANCH) === 1,
+  firstPush: !env.CIRCLE_PREVIOUS_BUILD_NUM,
   correctBuild: _.isEmpty(env.CI_PULL_REQUEST),
   uploadBuild: env.CIRCLE_NODE_INDEX === '0'
 }
