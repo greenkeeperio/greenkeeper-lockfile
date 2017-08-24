982c7eebba5a2c248ab6215ac0ee08bd85f16c13
Fabian Stegemann <fabian.stegemann@gmx.net>
2017-08-24 17:39:57 +0200

    feat: basic bitrise support

--- ci-services/tests.js
+++ ci-services/tests.js
@@ -4,5 +4,6 @@ module.exports = {
   circleci: () => env.CIRCLECI === 'true',
   jenkins: () => env.JENKINS_URL !== undefined,
   travis: () => env.TRAVIS === 'true',
-  wercker: () => env.WERCKER === 'true'
+  wercker: () => env.WERCKER === 'true',
+  bitrise: () => env.CI === 'true' && env.BITRISE_BUILD_NUMBER !== ''
 }
