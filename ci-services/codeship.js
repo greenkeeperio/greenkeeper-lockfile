7dcfdd647946eccc07ff55f49f3fd196a3732a6d
Selby Kendrick <selby.kendrick@gmail.com>
2017-09-12 14:45:47 -0400

    basic codeship support

---
+++ ci-services/codeship.js
@@ -0,0 +1,17 @@
+const gitHelpers = require('../lib/git-helpers')
+
+const env = process.env
+
+module.exports = {
+  // The GitHub repo slug
+  repoSlug: env.CI_REPO_NAME,
+  // The name of the current branch
+  branchName: env.CI_BRANCH,
+  // Is this the first push on this branch
+  // i.e. the Greenkeeper commit
+  firstPush: gitHelpers.getNumberOfCommitsOnBranch(env.CI_BRANCH) === 1,
+  // Is this a regular build
+  correctBuild: true,
+  // Should the lockfile be uploaded from this build
+  uploadBuild: true
+}
