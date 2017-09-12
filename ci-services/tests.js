8942367ec8ad235d656648497cc54a65af009797
Selby Kendrick <selby.kendrick@gmail.com>
2017-09-12 14:55:27 -0400

    fix bitrise false positive

--- ci-services/tests.js
+++ ci-services/tests.js
@@ -8,6 +8,6 @@ module.exports = {
   jenkins: () => env.JENKINS_URL !== undefined,
   travis: () => env.TRAVIS === 'true',
   wercker: () => env.WERCKER === 'true',
-  bitrise: () => env.CI === 'true' && env.BITRISE_BUILD_NUMBER !== '',
+  bitrise: () => env.CI === 'true' && env.BITRISE_BUILD_NUMBER !== undefined,
   codeship: () => env.CI_COMMIT_ID !== undefined
 }
