404fedd856f4dff42b1f19c84bc367b47b660147
Paul Cooper <paul@tictrac.com>
2017-09-19 14:19:10 +0100

    Git Branch should include greenkeeper prefix
    Updating to `branchName` to include the prefix, it is required in order for the upload/update scripts to function. See https://github.com/greenkeeperio/greenkeeper-lockfile/blob/master/update.js#L46

--- ci-services/jenkins.js
+++ ci-services/jenkins.js
@@ -5,7 +5,7 @@ const gitHelpers = require('../lib/git-helpers')
 
 module.exports = {
   gitUrl: env.GIT_URL,
-  branchName: _.drop(_.split(env.GIT_BRANCH, '/')).join('/'),
+  branchName: env.GIT_BRANCH,
   firstPush: gitHelpers.getNumberOfCommitsOnBranch(env.GIT_BRANCH) === 1,
   correctBuild: true, // assuming pull requests are not build
   uploadBuild: true // assuming 1 build per branch
