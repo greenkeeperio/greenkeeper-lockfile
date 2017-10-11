01b13be53378a52925671e8bdd786f363eb229f9
Christopher Eck <chris@coda.io>
2017-10-11 13:04:16 -0700

    Get greenkeeper-lockfile working for Jenkins pipelines.

--- ci-services/jenkins.js
+++ ci-services/jenkins.js
@@ -3,10 +3,18 @@ const env = process.env
 const _ = require('lodash')
 const gitHelpers = require('../lib/git-helpers')
 
+// Jenkins reports the branch name and Git URL in a couple of different places depending on use of the new
+// pipeline vs. older job types.
+const branchName = env.CHANGE_BRANCH || env.GIT_BRANCH
+const gitUrl = env.CHANGE_URL || env.GIT_URL
+
+// Build number is available in Jenkins pipeline builds; for others, defer to asking GIT.
+const buildNumber = env.BUILD_NUMBER || gitHelpers.getNumberOfCommitsOnBranch(branchName)
+
 module.exports = {
-  gitUrl: env.GIT_URL,
-  branchName: _.drop(_.split(env.GIT_BRANCH, '/')).join('/'),
-  firstPush: gitHelpers.getNumberOfCommitsOnBranch(env.GIT_BRANCH) === 1,
-  correctBuild: true, // assuming pull requests are not build
-  uploadBuild: true // assuming 1 build per branch
+  gitUrl,
+  branchName,
+  firstPush: buildNumber === 1,
+  correctBuild: true, // assuming this is always the correct build to update the lockfile
+  uploadBuild: true // assuming 1 build per branch/PR
 }
