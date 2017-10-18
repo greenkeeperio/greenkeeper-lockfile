41e1d44b4e5bb97e873524f8c8aa52110042186b
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-10-18 09:13:56 -0400

    feat: allow to configure build leader id via env variable (#57)
    Uses environment variable `BUILD_LEADER_ID`, if set, to determine the build leader. Allow to run `greenkeeper-lockfile-upload` on a specific Node version or Travis stage.

--- ci-services/travis.js
+++ ci-services/travis.js
@@ -13,5 +13,5 @@ module.exports = {
   // Is this a regular build
   correctBuild: env.TRAVIS_PULL_REQUEST === 'false',
   // Should the lockfile be uploaded from this build
-  uploadBuild: env.TRAVIS_JOB_NUMBER.endsWith('.1')
+  uploadBuild: env.TRAVIS_JOB_NUMBER.endsWith(`.${env.BUILD_LEADER_ID || 1}`)
 }
