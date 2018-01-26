b24ee8a168b684e75d62833d06dc3656c8862bd6
Julia Krüger <juliakbln@gmail.com>
2018-01-26 15:02:25 +0100

    fix(ci-services): circleci count number of commits

--- ci-services/circleci.js
+++ ci-services/circleci.js
@@ -1,13 +1,14 @@
 'use strict'
 
 const _ = require('lodash')
+const gitHelpers = require('../lib/git-helpers')
 
 const env = process.env
 
 module.exports = {
   repoSlug: `${env.CIRCLE_PROJECT_USERNAME}/${env.CIRCLE_PROJECT_REPONAME}`,
   branchName: env.CIRCLE_BRANCH,
-  firstPush: !env.CIRCLE_PREVIOUS_BUILD_NUM,
+  firstPush: gitHelpers.getNumberOfCommitsOnBranch(env.CIRCLE_BRANCH) === 1,
   correctBuild: _.isEmpty(env.CI_PULL_REQUEST),
   uploadBuild: env.CIRCLE_NODE_INDEX === `${env.BUILD_LEADER_ID || 0}`
 }
