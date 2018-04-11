a9b3e65ff9dd9b65a19f98ff3687ff2cf75f7211
Jan Lehnardt <jan@apache.org>
2018-04-11 16:30:00 +0200

    feat: handle monorepos
    BREAKING CHANGE: This now works with monorepos, it’s a big refactor,
    that’s why we make it a new semver-major release when it lands, just
    so that existing users won’t get tripped up by little changes.
    
    However, the design goal here is to be as backwards compatible as
    possible.

--- update.js
+++ update.js
@@ -3,35 +3,19 @@
 'use strict'
 
 const fs = require('fs')
+const path = require('path')
+const process = require('process')
 
 const relative = require('require-relative')
+const fg = require('fast-glob')
 
 const config = require('./lib/config')
 const extractDependency = require('./lib/extract-dependency')
-const updateLockfile = require('./lib/update-lockfile')
+const { updateLockfile, commitLockfile } = require('./lib/update-lockfile')
 const ci = require('./ci-services')
 
 module.exports = function update () {
   const info = ci()
-  const pkg = relative('./package.json')
-
-  const shrinkwrapExists = fs.existsSync('./npm-shrinkwrap.json')
-  const packageLockExists = fs.existsSync('./package-lock.json')
-  const yarnLockExists = fs.existsSync('./yarn.lock')
-
-  if (!(shrinkwrapExists || packageLockExists || yarnLockExists)) {
-    return console.error(
-      'Without either an "npm-shrinkwrap.json", "package-lock.json" or "yarn.lock" file present there is no need to run this script'
-    )
-  }
-
-  if (!info.correctBuild) {
-    return console.error('This build should not update the lockfile. It could be a PR, not a branch build.')
-  }
-
-  if (!info.branchName) {
-    return console.error('No branch details set, so assuming not a Greenkeeper branch')
-  }
 
   // legacy support
   if (config.branchPrefix === 'greenkeeper/' && info.branchName.startsWith('greenkeeper-')) {
@@ -46,17 +30,56 @@ module.exports = function update () {
     return console.error('Only running on first push of a new branch')
   }
 
-  const dependency = extractDependency(pkg, config.branchPrefix, info.branchName)
-
-  if (!dependency) {
-    return console.error('No dependency changed')
+  if (!info.correctBuild) {
+    return console.error('This build should not update the lockfile. It could be a PR, not a branch build.')
   }
 
-  updateLockfile(dependency, {
-    yarn: yarnLockExists,
-    npm: packageLockExists || shrinkwrapExists
-  })
+  if (!info.branchName) {
+    return console.error('No branch details set, so assuming not a Greenkeeper branch')
+  }
 
-  console.log('Lockfile updated')
+  const allPackageFiles = fg.sync('./**/package.json')
+  const doCommit = allPackageFiles.reduce((didChange, pkgJson) => {
+    const lockfilePath = path.dirname(pkgJson)
+    const previousDir = process.cwd()
+    process.chdir(lockfilePath)
+
+    const pkg = relative('./package.json')
+
+    const shrinkwrapExists = fs.existsSync('./npm-shrinkwrap.json')
+    const packageLockExists = fs.existsSync('./package-lock.json')
+    const yarnLockExists = fs.existsSync('./yarn.lock')
+
+    if (!(shrinkwrapExists || packageLockExists || yarnLockExists)) {
+      console.info(
+        `${pkgJson}: Without either an "npm-shrinkwrap.json", "package-lock.json" or "yarn.lock" file present there is no need to run this script`
+      )
+      process.chdir(previousDir)
+      return didChange
+    }
+
+    const dependency = extractDependency(pkg, config.branchPrefix, info.branchName)
+
+    if (!dependency) {
+      console.error(`${pkgJson}: No dependency changed`)
+      process.chdir(previousDir)
+      return didChange
+    }
+
+    updateLockfile(dependency, {
+      yarn: yarnLockExists,
+      npm: packageLockExists || shrinkwrapExists
+    })
+    process.chdir(previousDir)
+    return true
+  }, false)
+
+  if (doCommit) {
+    commitLockfile()
+  }
+
+  if (process.env.NODE_ENV && process.env.NODE_ENV !== 'test') {
+    console.log('Lockfile updated')
+  }
 }
 if (require.main === module) module.exports()
