1532bd79eba7260a97970dce3e19b0ba9c9e29c2
Christopher Williams <chris.a.williams@gmail.com>
2018-06-14 10:14:24 -0400

    Consult env vars in order: CHANGE_TARGET, CHANGE_BRANCH, BRANCH_NAME, GIT_BRANCH to resolve original branch

--- ci-services/jenkins.js
+++ ci-services/jenkins.js
@@ -5,7 +5,7 @@ const env = process.env
 // Jenkins reports the branch name and Git URL in a couple of different places depending on use of the new
 // pipeline vs. older job types.
 const gitUrl = env.CHANGE_URL || env.GIT_URL
-const origBranch = env.CHANGE_BRANCH || env.GIT_BRANCH
+const origBranch = env.CHANGE_TARGET || env.CHANGE_BRANCH || env.BRANCH_NAME || env.GIT_BRANCH
 
 // Different Jenkins plugins format the branch name differently
 const matchesGreenkeeper = origBranch.match(/greenkeeper.*/)
