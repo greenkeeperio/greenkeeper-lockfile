819bd167ce63c2f6802fd1a860f90140de9039b8
Bernhard Frick <baer95@users.noreply.github.com>
2019-06-06 16:22:38 +0200

    fix: repoSlug for GitLab CI (#231)
    * Fix repoSlug
    
    Fix a misunderstanding because Greenkeeper and GitLab define a "slug" differently.
    
    * add more debugging output
    
    * more debug output
    
    * add origin to checkout
    
    * cleanup

--- ci-services/gitlab.js
+++ ci-services/gitlab.js
@@ -3,7 +3,7 @@
 const env = process.env
 
 module.exports = {
-  repoSlug: env.CI_PROJECT_PATH_SLUG,
+  repoSlug: env.CI_PROJECT_PATH,
   branchName: env.CI_COMMIT_REF_NAME,
   correctBuild: /^greenkeeper\/.+/.test(env.CI_COMMIT_REF_NAME),
   uploadBuild: true
